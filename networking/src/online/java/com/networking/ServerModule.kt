8865896895a802f5c7c55ae8ee1a88203e2ad1d1
SantiagoFlynnUTN <santiago.flynn2@gmail.com>
2023-08-02 06:30:26 +0200

    Use build source sets to provide Api implementation

---
+++ networking/src/online/java/com/networking/ServerModule.kt
@@ -0,0 +1,34 @@
+package com.networking
+
+import com.networking.api.Api
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServerModule {
+
+    private const val LOCALHOST = "10.0.2.2:8080"
+
+    @Provides
+    @Singleton
+    fun providesApi(
+        client: Retrofit,
+    ): Api {
+        return Api.init(client)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGsonClient(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("http://$LOCALHOST")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
